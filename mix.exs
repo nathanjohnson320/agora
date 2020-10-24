af6508f599a6bc2b5ed4e2596cb5e0322cb14577
Nathn Johnson <nathanjohnson320@gmail.com>
2020-10-23 22:02:59 -0400

    1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Agora.MixProject do
   def project do
     [
       app: :agora,
-      version: "0.1.1",
+      version: "1.0.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
